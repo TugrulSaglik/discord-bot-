0bcba96153a08a28380ea2a170332c902083dfd4
TugrulSaglik <112172215+TugrulSaglik@users.noreply.github.com>
2023-01-02 12:41:59 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,54 +1,131 @@
 import discord
 import responses 
-from datetime import datetime, timedelta, time 
 import asyncio
+import os
+from dotenv import load_dotenv
+import dotenv
+import datetime
 
+#Loads .env file that carries the discord token
+
+load_dotenv("C:/Users/tugru/OneDrive/Masaüstü/Kodlama/discord.env")
 
  
 
         
-    
+#Defines the function that will run when bot is initiated    
 def run_discord_bot():    
-    TOKEN = "MTA0Mjc3ODgyMjAwNjgwMDM4NA.GHZWM3.GLbk3aH__2KpSBPpF37ZR3oHxa088APFcCLup8"    
+    TOKEN = os.getenv("DISCORD_TOKEN")    
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
     
+    #This function returns a variation of the message where emoji/emojis are removed from any new sent message
+    async def censor_emoji(message):
+      censored_message = message.replace("😃", "**[BU EMOJİ YASAKLANMIŞTIR]** ")
+      
+      return censored_message
     
-    @client.event 
-    async def on_ready():
-        client.loop.create_task(main())
+     
     
     @client.event
+    
+    #Defines a function that makes sure the bot is logged in so it can execute the following loops without any errors 
+    async def on_ready():
+        await client.wait_until_ready()
+        
+        
+        client.loop.create_task(main())
+        
+    
+    @client.event
+    
+    #Defines a function which instructs the bot on what to do when any new message is sent
     async def on_message(message):
+        if message.channel == client.get_channel(855450711356866610):
+          messagechannel = os.getenv("CHANNEL_MESSAGE")
+          os.environ["CHANNEL_MESSAGE"] = str(int(messagechannel) + 1)
+          dotenv.set_key("C:/Users/tugru/OneDrive/Masaüstü/Kodlama/discord.env", "CHANNEL_MESSAGE", os.environ["CHANNEL_MESSAGE"])
         if message.author == client.user:
-            return
+              return
         username = str(message.author)
         user_message = str(message.content)
-        channel = str(message.channel)
+        channel = message.channel
         list = ["borağ","niggers","zzzzz"]
         msg_content = message.content.lower()
-        message_to_send = responses.handle_response(msg_content)
-        await channel.send(message_to_send)
+        
+        #Checks if the keywords are in the message, then decides to delete it or not
         if any(word == msg_content for word in list):
          await message.delete()
-              
+         
+      
+        """
+        Checks if the censored emoji/emojis are in the message's content.
+        If its in the content, then it deletes the original message and sends a modified version of it
+        """
+        if "😃" in message.content:
+          
+            user_id = message.author.id
+            await message.delete()
+            censored_message = await censor_emoji(message.content)
+            await message.channel.send(f"<@{user_id}> şahsının **düzeltilmiş** mesajı: " + censored_message)
+        
+        
+        #If no other condition is met, it sends the result it gets from the handle_response function  
+        else:
+          await channel.send(responses.handle_response(message.content))
+    
     
     @client.event
+    
+    #Defines a function which sends "I hate niggers" every 6 hours in a specific channel
     async def interval_task(channel): 
       await channel.send("I hate niggers")
 
     async def main():
-        channel = client.get_channel(855450711356866610)
-    
-        interval = 21600
-        while True:
-        
+      channel = client.get_channel(855450711356866610)
+      await ping_ozan()
+      interval = 21600
+      while True:
           await interval_task(channel)
-        
           await asyncio.sleep(interval)
+          
+    @client.event
+    async def on_message_edit(before, after):
+    # Check if the edited message contains an emoji that is not allowed
+      if "😃" in after.content:
+       # Get the user's ID
+       user_id = after.author.id
+       # Delete the original message
+       await after.delete()
+       # Censor the message
+       censored_message = await censor_emoji(after.content)
+       # Send the censored message
+       await after.channel.send(f"<@{user_id}> şahsının **düzeltilmiş** mesajı: " + censored_message)
+       if after.channel == client.get_channel(855450711356866610):
+          messagechannel = os.getenv("CHANNEL_MESSAGE")
+          os.environ["CHANNEL_MESSAGE"] = str(int(messagechannel) - 1)
+          dotenv.set_key("C:/Users/tugru/OneDrive/Masaüstü/Kodlama/discord.env", "CHANNEL_MESSAGE", os.environ["CHANNEL_MESSAGE"])
 
+    @client.event
+    async def ping_ozan():
+         channel = client.get_channel(855450711356866610)
+         message_count = 0
+         async for message in channel.history(limit=1000):
+          timestamp = message.created_at
+          formatted_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+          if "😃" in message.content:
+           censored_message = await censor_emoji(message.content)
+           await channel.send(f"<@{message.author.id}> şahsının **düzeltilmiş** mesajı: " + censored_message + "\n" +"\n" + "\n" + formatted_timestamp + "   (yazılan zaman türkiye saatinden 3 saat geridedir.)")
+           await message.delete()
+           if message.channel == client.get_channel(855450711356866610):
+            messagechannel = os.getenv("CHANNEL_MESSAGE")
+            os.environ["CHANNEL_MESSAGE"] = str(int(messagechannel) - 1)
+            dotenv.set_key("C:/Users/tugru/OneDrive/Masaüstü/Kodlama/discord.env", "CHANNEL_MESSAGE", os.environ["CHANNEL_MESSAGE"])
+          
+          message_count += 1
+          print("{0:.1%} tamamlandi :)!".format(message_count/1000))
     
-        
+  
     
-    client.run(TOKEN)   
+    client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,10 +1,33 @@
 import string
+from random import randint
+import re
+import os
+from dotenv import load_dotenv
+import discord
+
+load_dotenv("C:/Users/tugru/OneDrive/Masaüstü/Kodlama/discord.env")
 
 
-def handle_response(message) -> str:
+#Checks if there is a pattern of several letters line up in a specific way with nothing before and after the sequence
+def check_for_zort(message):
+  pattern = r"\bz+o+r+t\b"  
+  if re.search(pattern, message, re.IGNORECASE):
+    return True
+  return False  
+
+#The function that determines how to respond to any given message
+def handle_response(message):
+  
+  #Message is converted into lowercase version so more variations of the same message could be recognized as the same
     p_message = message.lower()
     
-    if "max" in p_message:
+    """
+    The following if statement checks if the first three indexes of the message is "max". 
+    If it is, and there is no such file as "yarrak.txt" then it creates such a file with an integer of 0, then adds 1 to it and saves.
+    Or, if there is such a file as "yarrak.txt" then it increments by 1 and saves it.
+    """
+    if p_message[0] == "m" and p_message[1]== "a" and p_message[2] == "x":
+      
         f = open("yarrak.txt",'r') 
         data = f.read() 
         counter = int(data)   
@@ -15,6 +38,11 @@ def handle_response(message) -> str:
         a.close()
         return message
     
+    """
+    The following if statements checks if the data on "yarrak.txt" is greater then zero.
+    If it isn't, it creates a variable as "counter" with a value of 0, and then it returns a string.
+    If it is greater than zero, then it creates a variable equal to the integer and then returns a string.
+    """
     if p_message == "counter":
         f = open("yarrak.txt",'r') 
         data = f.read() 
@@ -35,11 +63,31 @@ def handle_response(message) -> str:
     if p_message == "zzzzz":
       return "documentation yok ozan sorma"
     
-    if p_message == "mahmut, ozan hakkında ne düşünüyorsun?":
-      return "OZAN MI????? O acayip cringe birisi ve 1984 gibi benim ne yaptığımı bilmek istiyor hayır okumadım kitabı ama..."
+    if p_message == "yusuf ananı sikebilir miyim":
+      return "evet efendim, serbestçe sikebilirsiniz annemi."
     
     if p_message == "mahmut, tuğrul hakkında ne düşünüyorsun?":
       return "direkt allahım"
     
     if p_message == "mahmut, bora hakkında ne düşünüyorsun?":
       return "based birisi ama underage arkadaşları o kadar based değil"
+    
+    if p_message == "iyi geceler yusuf":
+      return "iyi geceler 😏 🤙 "  
+    
+    if check_for_zort(p_message) == True:
+      zort_answers = ["zort ", "zarttiri zort zort ", "ZORT ", "zzzzzORT ", "ZART ZURT ","lol ZORT "]
+      if "bora" in p_message:
+        possible_bora = ["bora", "boraĞ", "söğü", "bora güneş", "ğağeş", "güneş bora", "söğü ğaneş"]
+        return zort_answers[randint(0,5)] + possible_bora[randint(0,6)]
+      elif "ozan" in p_message:
+        possible_ozan = ["ozan","ozi","ozan ölmez", "ozan DÖNMEZ", "ozi mozi", "ozan ölmez kuşu ötmez"]
+        return zort_answers[randint(0,5)] + possible_ozan[randint(0,5)]
+      elif "tuğrul" in p_message:
+        possible_tuğrul = ["tugug", "tuğrul", "tuğrul SAĞLIK", "tugug salak","ibo tugug saĞ"]
+        return zort_answers[randint(0,5)] + possible_tuğrul[randint(0,4)]
+      return zort_answers[randint(0,5)]
+    
+    if p_message == "!mesajsayısı":
+      toplam_mesajlar = os.getenv("CHANNEL_MESSAGE") 
+      return "Toplamda #genel kanalında {} tane mesaj var!".format(toplam_mesajlar)
